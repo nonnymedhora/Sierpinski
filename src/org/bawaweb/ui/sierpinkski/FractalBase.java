9deed9132828ed9328c998781d1764cde28346b3
nonnymedhora <w84meto>
2019-08-11 13:13:47 +0530

    FractalBaseSample extracted, 'main' method moved from FractalBase

--- src/org/bawaweb/ui/sierpinkski/FractalBase.java
+++ src/org/bawaweb/ui/sierpinkski/FractalBase.java
@@ -3,7 +3,6 @@
  */
 package org.bawaweb.ui.sierpinkski;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -14,57 +13,7 @@ import java.awt.image.BufferedImage;
 import java.util.Objects;
 
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 
-
-class FractalBaseSample extends FractalBase {
-	
-	public FractalBaseSample(){
-		super();
-	}
-
-	private static final long serialVersionUID = 1L;
-
-	@Override
-	public void createFractalShape(Graphics2D g) {
-		final Point center = new Point((WIDTH - OFFSET * 2) / 2, (HEIGHT - OFFSET * 2) / 2);
-		// System.out.println("Center is P(" + center.x + "," + center.y + ")");
-
-		g.setStroke(new BasicStroke(4));
-		setPixel(center.y, center.x, Color.cyan.getRGB());
-
-		final int length = 50 * depth + 1;
-//		System.out.println("length is " + length);
-		
-		g.setStroke(new BasicStroke(1));
-		drawSquare(g, center, length * 2, Color.cyan);
-
-		g.setColor(Color.green);
-		drawSquare(g, center, length);
-
-		g.setColor(Color.red);
-		drawCircle(g, center, length);
-
-		drawCircle(g, center, length / 2, Color.yellow);
-		fillCircle(g, center, length / 3, Color.green);
-		fillSquare(g, center, length / 4, Color.blue);
-
-//		g.setStroke(new BasicStroke(2));
-		g.setColor(Color.white);
-		drawEquilateralTriangle(g, center, length * 2, "up");
-		drawEquilateralTriangle(g, center, length * 2, "down");
-
-		g.setColor(Color.red);
-		drawEquilateralTriangle(g, center, length, "up");
-		drawEquilateralTriangle(g, center, length, "down");
-
-		g.setColor(Color.red);
-		fillEquilateralTriangle(g, center, length / 6, "up");
-		fillEquilateralTriangle(g, center, length / 6, "down");
-		
-	}
-	
-}
 
 /**
  * @author Navroz
@@ -72,28 +21,9 @@ class FractalBaseSample extends FractalBase {
  */
 public abstract class FractalBase extends JFrame implements Runnable {
 	
-	public static void main(String[] arf) {
-		final FractalBase ff = new FractalBaseSample();
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				final FractalBase frame = ff;
-				frame.setTitle("Bawaz _ FractalBaseSample");
-				frame.setSize(frame.WIDTH, frame.HEIGHT);
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setResizable(false);
-				frame.setVisible(true);
-
-				new Thread(frame).start();
-
-			}
-		});
-	}
-	
-	
 	private static final long serialVersionUID = 123456543L;
-	static final int HEIGHT = 600;
-	static final int WIDTH = 600;
+	protected static final int HEIGHT = 600;
+	protected static final int WIDTH = 600;
 
     boolean isOriginUpperLeft = true;  	// location of origin
 	static int OFFSET = 25; 			// pixel offset from edge
---
+++ src/org/bawaweb/ui/sierpinkski/FractalBaseSample.java
@@ -0,0 +1,76 @@
+package org.bawaweb.ui.sierpinkski;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+class FractalBaseSample extends FractalBase {
+	
+	public FractalBaseSample(){
+		super();
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void createFractalShape(Graphics2D g) {
+		final Point center = new Point((WIDTH - OFFSET * 2) / 2, (HEIGHT - OFFSET * 2) / 2);
+		// System.out.println("Center is P(" + center.x + "," + center.y + ")");
+
+		g.setStroke(new BasicStroke(4));
+		setPixel(center.y, center.x, Color.cyan.getRGB());
+
+		final int length = 50 * depth + 1;
+//		System.out.println("length is " + length);
+		
+		g.setStroke(new BasicStroke(1));
+		drawSquare(g, center, length * 2, Color.cyan);
+
+		g.setColor(Color.green);
+		drawSquare(g, center, length);
+
+		g.setColor(Color.red);
+		drawCircle(g, center, length);
+
+		drawCircle(g, center, length / 2, Color.yellow);
+		fillCircle(g, center, length / 3, Color.green);
+		fillSquare(g, center, length / 4, Color.blue);
+
+//		g.setStroke(new BasicStroke(2));
+		g.setColor(Color.white);
+		drawEquilateralTriangle(g, center, length * 2, "up");
+		drawEquilateralTriangle(g, center, length * 2, "down");
+
+		g.setColor(Color.red);
+		drawEquilateralTriangle(g, center, length, "up");
+		drawEquilateralTriangle(g, center, length, "down");
+
+		g.setColor(Color.red);
+		fillEquilateralTriangle(g, center, length / 6, "up");
+		fillEquilateralTriangle(g, center, length / 6, "down");
+		
+	}
+
+	public static void main(String[] arf) {
+		final FractalBase ff = new FractalBaseSample();
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				final FractalBase frame = ff;
+				frame.setTitle("Bawaz _ FractalBaseSample");
+				frame.setSize(FractalBase.WIDTH, FractalBase.HEIGHT);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setResizable(false);
+				frame.setVisible(true);
+	
+				new Thread(frame).start();
+	
+			}
+		});
+	}
+	
+}
