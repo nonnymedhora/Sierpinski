789062f0b1a18795acce2b905d3e2e2f69613d2e
nonnymedhora <w84meto>
2019-08-13 13:03:28 +0530

    added method - startFractals

--- src/org/bawaweb/ui/sierpinkski/SierpinskiCompositional.java
+++ src/org/bawaweb/ui/sierpinkski/SierpinskiCompositional.java
@@ -219,29 +219,34 @@ class SierpinskiComboPanel extends JPanel {
 		}
 
 		ff.reset();
-		final String title = ff.getFractalShapeTitle();
+
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-				final FractalBase frame = ff;
-				frame.setTitle(title);
-				frame.setSize(FractalBase.WIDTH, FractalBase.HEIGHT);
-				frame.setDefaultCloseOperation(closeIt(frame));
-				frame.setResizable(false);
-				frame.setVisible(true);
+				startFractals(ff);
 
-				fbf = new Thread(frame);
-				fbf.start();
-
-			}
-
-			private int closeIt(FractalBase frame) {
-				frame.reset();
-				frame.dispose();
-				return JFrame.DISPOSE_ON_CLOSE;
 			}
 		});
 	}	
+
+
+	private void startFractals(final FractalBase ff) {
+		final FractalBase frame = ff;
+		frame.setTitle(ff.getFractalShapeTitle());
+		frame.setSize(FractalBase.WIDTH, FractalBase.HEIGHT);
+		frame.setDefaultCloseOperation(closeIt(frame));
+		frame.setResizable(false);
+		frame.setVisible(true);
+
+		this.fbf = new Thread(frame);
+		this.fbf.start();
+	}
+
+	private int closeIt(FractalBase frame) {
+		frame.reset();
+		frame.dispose();
+		return JFrame.DISPOSE_ON_CLOSE;
+	}
 	
 	private void doPauseCommand(){
 		this.fbf.interrupt();
@@ -256,23 +261,7 @@ class SierpinskiComboPanel extends JPanel {
 				JComboBox<String> cb = (JComboBox<String>)e.getSource();
 		        String comboOption = (String)cb.getSelectedItem();
 				doSelectCombosCommand(comboOption);				
-			}
-
-//			private void doSelectCombosCommand(String option) {
-//				comboChoice = option;
-//
-//				if (comboChoice.equals(APOLLONIAN_CIRCLES)) {
-//					fannyOptionsPanel.setVisible(false);
-//					apolloOptionsPanel.setVisible(true);
-//				} else if (comboChoice.equals(FANNY_CIRCLE) || comboChoice.equals(FANNY_TRIANGLES)) {
-//					fannyOptionsPanel.setVisible(true);
-//					apolloOptionsPanel.setVisible(false);
-//				} else {
-//					fannyOptionsPanel.setVisible(false);
-//					apolloOptionsPanel.setVisible(false);
-//				}
-//			}	    	
-	    });
+			}});
 		
 		this.sideCombos.addActionListener(new ActionListener() {
 			@SuppressWarnings("unchecked")
@@ -283,12 +272,7 @@ class SierpinskiComboPanel extends JPanel {
 //		        System.out.println("sideComboOption==="+sideComboOption);
 		        int sideVal = cb.getItemAt(sideComboOption);
 				doSelectSideCombosCommand(sideVal);				
-			}
-
-//			private void doSelectSideCombosCommand(Integer sideOption) {
-//				sideChoice=sideOption;
-//			}	    	
-	    });
+			}});
 		
 		this.ratioCombos.addActionListener(new ActionListener() {
 			@SuppressWarnings("unchecked")
@@ -300,12 +284,7 @@ class SierpinskiComboPanel extends JPanel {
 //		        System.out.println("ratioComboOption==="+ratioComboOption);
 		        int ratioVal = cb.getItemAt(ratioComboOption);
 				doSelectRatioCombosCommand(ratioVal);				
-			}
-
-//			private void doSelectRatioCombosCommand(Integer ratioOption) {
-//				ratioChoice=ratioOption;
-//			}	    	
-	    });
+			}});
 		
 		this.curvCombos.addActionListener(new ActionListener() {
 			@SuppressWarnings("unchecked")
@@ -315,111 +294,13 @@ class SierpinskiComboPanel extends JPanel {
 				String curvComboOption = (String)cb.getSelectedItem();
 //		        System.out.println("curvComboOption==="+curvComboOption);
 				doSelectCurvCombosCommand(curvComboOption);				
-			}
-
-//			private void doSelectCurvCombosCommand(String cOption) {
-//				switch (cOption) {
-//					case A1: // A1 = "C[1, 1, 1] M[1]";
-//						curvChoices = new double[] { 1.0, 1.0, 1.0 };
-//						mult = 1.0;
-//						break;
-//	
-//					case A2: // A2 = "C[25, 25, 28] M[20]";
-//						curvChoices = new double[] { 25.0, 25.0, 28.0 };
-//						mult = 20.0;
-//						break;
-//	
-//					case A3: // A3 = "C[50, 80, 8] M[6]";
-//						curvChoices = new double[] { 50.0, 80.0, 8.0 };
-//						mult = 6.0;
-//						break;
-//	
-//					case A4: // A4 = "C[10, 15, 19] M[6]";
-//						curvChoices = new double[] { 10.0, 15.0, 19.0 };
-//						mult = 6.0;
-//						break;
-//	
-//					case A5: // A5 = "C[23, 27, 18] M[16]";
-//						curvChoices = new double[] { 23.0, 27.0, 16.0 };
-//						mult = 16.0;
-//						break;
-//	
-//					case A6: // A6 = "C[2, 2, 3] M[1]";
-//						curvChoices = new double[] { 2.0, 2.0, 3.0 };
-//						mult = 1.0;
-//						break;
-//	
-//					default:
-//						// use--a1
-//						curvChoices = new double[] { 1.0, 1.0, 1.0 };
-//						mult = 1.0;
-//						break;
-//				}
-//			}	    	
-	    });
+			}});
 		
 		this.buStart.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				doStartCommand();				
-			}
-
-//			private void doStartCommand() {
-//				String choice = getComboChoice();
-//				int length = getSideComboChoice();
-//				int ratio = getRatioChoice();
-////				System.out.println("choice is " + choice + " and length is " + length + " and ratio is " + ratio);
-//				final FractalBase ff;
-//				if (choice.equals(FANNY_CIRCLE)) {
-//					ff = new FannyCircle(length, ratio);
-//				} else if (choice.equals(FANNY_TRIANGLES)) {
-//					ff = new FannyTriangles(length, ratio);
-//				} else if (choice.equals(SIERPINSKI_TRIANGLES)) {
-//					ff = new SierpinskiTriangle();
-//				} else if (choice.equals(SIERPINSKI_SQUARES)) {
-//					ff = new SierpinskiSquare();
-//				} else if (choice.equals(APOLLONIAN_CIRCLES)) {
-//					ff = new ApollonianCircles(curvChoices, mult);
-//				} else if (choice.equals(SAMPLE)) {
-//					ff = new FractalBaseSample();
-//				} else if (choice.equals(MANDELBROT)) {
-//					ff = new Mandelbrot();
-//				} else if (choice.equals(CST_FRACTAL)) {
-//					ff = new CSTFractal();
-//				} else if (choice.equals(SAMPLE)) {
-//					ff = new FractalBaseSample();
-//				} else if (choice.equals(KOCHSNOWFLAKE)) {
-//					ff = new KochSnowFlakeFractal();
-//				} else {
-//					ff = null;
-//					return;
-//				}
-//
-//				ff.reset();
-//				final String title = ff.getFractalShapeTitle();
-//				SwingUtilities.invokeLater(new Runnable() {
-//					@Override
-//					public void run() {
-//						final FractalBase frame = ff;
-//						frame.setTitle(title);
-//						frame.setSize(FractalBase.WIDTH, FractalBase.HEIGHT);
-//						frame.setDefaultCloseOperation(closeIt(frame));
-//						frame.setResizable(false);
-//						frame.setVisible(true);
-//			
-//						new Thread(frame).start();
-//			
-//					}
-//
-//					private int closeIt(FractalBase frame) {
-//						frame.reset();
-//						frame.dispose();
-//						return JFrame.DISPOSE_ON_CLOSE;
-//					}
-//				});
-//			}	    	
-	    });
-		
+			}});		
 
 		this.buPause.addActionListener(new ActionListener() {
 			@Override
@@ -496,7 +377,7 @@ public class SierpinskiCompositional extends JFrame {
 		        frame.setResizable(false);
 		        frame.setVisible(true);
 		      }
-		    });
+		});
 	}
 
 }
