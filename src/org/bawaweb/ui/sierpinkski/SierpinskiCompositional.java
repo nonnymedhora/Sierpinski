a5977c6b6e7ae1ee985ace549aa74f4c2bc4090a
nonnymedhora <w84meto>
2019-09-13 23:36:14 +0530

    Added Image info in Save Image

--- src/org/bawaweb/ui/sierpinkski/SierpinskiCompositional.java
+++ src/org/bawaweb/ui/sierpinkski/SierpinskiCompositional.java
@@ -5,12 +5,17 @@ package org.bawaweb.ui.sierpinkski;
 
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -1947,16 +1952,52 @@ System.out.println("this.comboChoice--"+this.comboChoice+"isThread");
 		fb.interrupt();
 	}
 	
-	private void doSaveImageCommand(){
-		BufferedImage image = this.getFractalImage();
+	private void doSaveImageCommand() {
+		BufferedImage fractalImage = this.getFractalImage();
+		
+		
 		String extraInfo = this.getExtraInfo();
+		
+		BufferedImage extraInfoImg = this.createImageInfo(extraInfo);
+        
+        
 		String imageFilePath="images\\"+this.getComboChoice()+"["+extraInfo+"]"+" ____"+System.currentTimeMillis()+".png";
 		File outputfile = new File(imageFilePath);
 	    try {
-			ImageIO.write(image, "png", outputfile);
+			ImageIO.write(FractalBase.joinBufferedImage(fractalImage,extraInfoImg), "png", outputfile);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private BufferedImage createImageInfo(String extraInfo) {
+		BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = img.createGraphics();
+        Font font = new Font("Arial", Font.PLAIN, 18);
+        g2d.setFont(font);
+        FontMetrics fm = g2d.getFontMetrics();
+        int width = fm.stringWidth(extraInfo);
+        int height = fm.getHeight();
+        g2d.dispose();
+        
+        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        g2d = img.createGraphics();
+       /* g2d.setColor(Color.BLACK);
+        g2d.fillRect(0,0,width,height);*/
+        g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+        g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+        g2d.setFont(font);
+        fm = g2d.getFontMetrics();
+        g2d.setColor(Color.RED);
+        g2d.drawString(extraInfo, 0, fm.getAscent());
+        g2d.dispose();
+		return img;
 	}
 	
 	private String getExtraInfo() {
